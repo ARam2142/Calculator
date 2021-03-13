d7256dd5fe2158d518993864082aba39a6b6c036
Aram2142 <andresaaramirez@aol.com>
2021-03-12 21:31:06 -0500

    numbers appear on screen

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 //grab all elements, make sure all buttons click
+const showNum = document.querySelector('.screen_display'); //console.log(screen)
+//const buttons = document.querySelectorAll('.calcbutton');console.log(buttons);
+const numbers = document.querySelectorAll('.number'); console.log(numbers)
+let displayResult = false;
+
+numbers.forEach(num => {
+    num.addEventListener("click", e => {
+        displayResult = true;
+        let target = e.target;
+        let targetNum = target.textContent; console.log(targetNum)
+        let displayNum = showNum.innerHTML = targetNum;
+
+        let theNum = 0
+        while (console.log(theNum < 10)) {
+            console.log(theNum++)
+            theNum = target + displayNum
+        }
+        
+        
+        
+    })
+})
+
+
 
