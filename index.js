dfca88f1ef670c373ef57df40c47c7b80e0fb8d6
Aram2142 <andresaaramirez@aol.com>
2021-03-19 22:55:28 -0400

    clear button resets to 0

--- index.js
+++ index.js
@@ -2,46 +2,56 @@ const numberButton = document.querySelectorAll('.number'); //console.log(numbers
 const operatorButton = document.querySelectorAll('.operator');
 const toolButton = document.querySelectorAll('.others');
 const decimalButton = document.querySelector('.decimal');
+const clearBtn = document.querySelector('.reset')
 const screenDisplay = document.querySelector('.screen_display')
 
 let display, number, zero,
-    equal = false;
+operator, equal = false;
 let valEntered = false;
 
 numberButton.forEach(num => {
     num.addEventListener("click", (e) => {
-        let str = screenDisplay.textContent
-        if (Number(str)===0&& str.indexOf('.') <= -1) {
-            display = screenDisplay.textContent = str.replace(/^0+/, "");    
+        let str = screenDisplay.textContent;
+        if (!str.includes(".")) {
+            display = screenDisplay.textContent = str.replace(/^0+/, "");
         }
         if (valEntered) {
             display = screenDisplay.textContent = e.target.value;
         } else {
             display = screenDisplay.textContent += e.target.value;
         }
-
-        if (display.length > 16) {
-            alert("Thats as far as it goes");
-        }
+        
     })
 });
 
 decimalButton.addEventListener('click', (e) => {
     let decimal = e.target.value;
-    zero = '0.'
-    if (zero) {
-        display = screenDisplay.textContent += decimal;
+    let screenStr = screenDisplay.textContent;
+    decimalButton.disabled = true
+    if (screenStr.indexOf('.') > -1) {
+        display = screenDisplay.textContent = screenStr.search(/^\d+(\.\d+)?$/); 
     }
-
+    if (decimal) {
+        display = screenDisplay.textContent += decimal
+    }
+    
+    
 });
+
 
 operatorButton.forEach(op => {
     op.addEventListener('click', () => {
-        let butOp = op.textContent; console.log(butOp);
+        let strOnScreen = /^(\d+(\.\d+)?)$/;
 
+        let butOp = op.textContent; console.log(butOp);
+        
     })
 })
 
+clearBtn.addEventListener('click', (e)=> {
+    display = screenDisplay.textContent = "0";
+    decimalButton.disabled = false;
+})
 
 
 
